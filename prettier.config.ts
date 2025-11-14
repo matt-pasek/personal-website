a516b7dd899e5f37e604a1f2cf38267f64ce73c4
matt-pasek <m_pasek@icloud.com>
2025-11-15 01:20:27 +0200

    prettier config

---
+++ prettier.config.ts
@@ -0,0 +1,17 @@
+import { type Config } from 'prettier';
+
+const config: Config = {
+  bracketSpacing: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 120,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  semi: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+  endOfLine: 'crlf',
+  tailwindStylesheet: 'src/app/globals.css',
+};
+
+export default config;
