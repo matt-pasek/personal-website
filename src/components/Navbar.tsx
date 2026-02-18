46fce0288a005afde433ce9240af194130394f74
matt-pasek <m_pasek@icloud.com>
2026-02-18 23:51:17 +0200

    changed size of navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,8 @@
 export const Navbar = () => {
   return (
-    <div className="absolute top-0 left-0 h-16 w-full items-center p-4 text-white">
-      <div className="mx-auto flex w-full max-w-3/4 items-center justify-between">
-        <div className="text-2xl font-bold">Mateusz Pasek</div>
+    <div className="absolute top-6 left-0 h-16 w-full items-center p-4 text-white">
+      <div className="mx-auto flex w-3/5 min-w-6xl items-center justify-between">
+        <div className="text-3xl font-bold">Mateusz Pasek</div>
         <nav>
           <ul className="flex space-x-6">
             <li>
