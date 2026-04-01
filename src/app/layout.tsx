50b84e8b5c6d71f1157f423b2a39a430dc0cae04
matt-pasek <m_pasek@icloud.com>
2026-04-01 11:33:11 +0300

    made background nicer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,6 @@ export default function RootLayout({
     <html lang="en">
       <body className="antialiased">
         <BlobStateProvider>
-          <Background />
           <Navbar />
           {children}
         </BlobStateProvider>
